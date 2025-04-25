e07659e65f7b5cbad743d9d6426b9fb2157bb10a
Leonid Fridlikh <lfridlikh@inno.tech>
2025-04-25 20:08:11 +0300

    Two digit calculation added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
-    }
+import java.util.Scanner;
+public class Main { public static void main(String[] args) { System.out.println("Введите первое число");
+    int firstNumber = new Scanner(System.in).nextInt();
+    System.out.println("Введите второе число");
+    int secondNumber = new Scanner(System.in).nextInt();
+    double add = (double) firstNumber + secondNumber;
+    double sub = (double) firstNumber - secondNumber;
+    double mul = (double) firstNumber * secondNumber;
+    double div = (double) firstNumber / secondNumber;
+    System.out.println("Сумма: " + (int)add);
+    System.out.println("Разность: " + (int)sub);
+    System.out.println("Произведение: " + (int)mul);
+    if ((firstNumber % secondNumber) !=0) {
+        System.out.println("Частное: " + div);}
+    else {
+        System.out.println("Частное: " + (int)div);}
+}
 }
